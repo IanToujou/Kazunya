ea477d2af66c23d4f9dba8f432c2e857bfbfb56d
IanToujou <ianbour2016@gmail.com>
2021-04-12 18:16:46 +0200

    Added new gifs

--- src/main/java/net/toujoustudios/kazunya/command/list/roleplay/FuckCommand.java
+++ src/main/java/net/toujoustudios/kazunya/command/list/roleplay/FuckCommand.java
@@ -17,6 +17,7 @@ import java.util.Random;
  * Date: 11/04/2021
  * Time: 22:51
  * Project: Kazunya
+ * WARNING: THIS FILE CONTAINS NSFW MATERIAL (LINKS, IMAGES, ETC.)
  */
 public class FuckCommand implements ICommand {
 
@@ -29,6 +30,16 @@ public class FuckCommand implements ICommand {
         Member member = context.getMember();
 
         embedBuilder.setColor(Config.DEFAULT_EMBED_COLOR);
+
+        if(!channel.isNSFW()) {
+
+            embedBuilder.setTitle("**__ERROR__**");
+            embedBuilder.setColor(Config.ERROR_EMBED_COLOR);
+            embedBuilder.setDescription(":x: You can only use this command in **NSFW** channels.");
+            channel.sendMessage(embedBuilder.build()).queue();
+            return;
+
+        }
 
         if(args.size() == 0) {
 
@@ -53,7 +64,21 @@ public class FuckCommand implements ICommand {
         Member target = context.getMessage().getMentionedMembers().get(0);
 
         ArrayList<String> images = new ArrayList<>();
-        images.add("");
+        images.add("https://img.rule34.xxx//images/3529/17a48932b647d0389f1a972ef16f3155.gif?3984981");
+        images.add("https://wimg.rule34.xxx//images/4036/641fb57bfbca715f8651dbe514466145.gif?4579111");
+        images.add("https://wimg.rule34.xxx//images/4036/5ba4723d74fb25df5153108859892f4f.gif?4579057");
+        images.add("https://wimg.rule34.xxx//images/4022/1015b2c336c4dde479b2be9eb07f9ab4.gif?4564914");
+        images.add("https://img.rule34.xxx//images/4022/102669b29d3759a50f2e52b65ea9c991.gif?4564930");
+        images.add("https://wimg.rule34.xxx//images/3918/7033640885226ab02465048171301087.gif?4439966");
+        images.add("https://wimg.rule34.xxx//images/3875/95601b118df57b34205ee9a1b5a69dcc.gif?4387832");
+        images.add("https://img.rule34.xxx//images/3872/72f0c160da37d13003e3e806efdec08c.gif?4383529");
+        images.add("https://wimg.rule34.xxx//images/3860/80fb17d555d4611b606ceed4efdd9d4b.gif?4370018");
+        images.add("https://wimg.rule34.xxx//images/3860/c7fc3098e2883f9821d39820927818e6.gif?4369988");
+        images.add("https://wimg.rule34.xxx//images/3858/bcb37527daa4719723e33ac28ff03fef.gif?4367632");
+        images.add("https://img.rule34.xxx//images/3838/fcbe59663984549e4be534d60bc98921.gif?4345553");
+        images.add("https://wimg.rule34.xxx//images/3835/76d95b595a5dce90d0cd1c776db544e1a62e7028.gif?4342424");
+        images.add("https://wimg.rule34.xxx//images/3835/84f7893a2c269396b87056eb9ee6199cc99c9f22.gif?4342422");
+        images.add("https://wimg.rule34.xxx//images/3689/3f578258dc13086b6f70997fbc1a661a.gif?4173542");
 
         embedBuilder.setDescription("**" + member.getEffectiveName() + "** is fucking **" + target.getEffectiveName() + "**! :sweat_drops:");
         embedBuilder.setImage(images.get(new Random().nextInt(images.size())));

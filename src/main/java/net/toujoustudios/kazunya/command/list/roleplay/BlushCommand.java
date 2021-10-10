9a46f92296f810078c7215243becd551fed04dda
= <ianbour2016@gmail.com>
2021-10-10 19:16:26 +0200

    Added blush GIFs

--- src/main/java/net/toujoustudios/kazunya/command/list/roleplay/BlushCommand.java
+++ src/main/java/net/toujoustudios/kazunya/command/list/roleplay/BlushCommand.java
@@ -12,8 +12,10 @@ import net.toujoustudios.kazunya.config.Config;
 import net.toujoustudios.kazunya.error.ErrorEmbed;
 import net.toujoustudios.kazunya.error.ErrorType;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 /**
  * This file has been created by Ian Toujou.
@@ -41,11 +43,35 @@ public class BlushCommand implements ICommand {
             return;
         }
 
-        //ArrayList<String> images = new ArrayList<>();
-        //images.add("");
+        ArrayList<String> images = new ArrayList<>();
+        images.add("https://c.tenor.com/lDz32d62I0sAAAAC/aikatsu-stars.gif");
+        images.add("https://c.tenor.com/T51BLj_Cj8cAAAAC/blush.gif");
+        images.add("https://c.tenor.com/HBjElxuQjuIAAAAC/anime-blush.gif");
+        images.add("https://c.tenor.com/KKLSi3AgXuoAAAAC/anime-girl.gif");
+        images.add("https://c.tenor.com/HAWlr1X00Y8AAAAC/anime-love.gif");
+        images.add("https://c.tenor.com/VrfSZUjiWn4AAAAC/shy-anime.gif");
+        images.add("https://c.tenor.com/eBtzvs6BwisAAAAC/anime-girl.gif");
+        images.add("https://c.tenor.com/cjFjOFwZODIAAAAC/anime-girl.gif");
+        images.add("https://c.tenor.com/JhO1fYhvP14AAAAC/face-blush.gif");
+        images.add("https://c.tenor.com/d3AEjdxSfawAAAAC/anime-blush.gif");
+        images.add("https://c.tenor.com/QRtXsJdPJsoAAAAC/sad-eyes-anime.gif");
+        images.add("https://c.tenor.com/M7wcdD0eujYAAAAd/anime-looking.gif");
+        images.add("https://c.tenor.com/FHjoCc579pAAAAAC/blush-blushing.gif");
+        images.add("https://c.tenor.com/wwxHnJqUNEMAAAAC/anime-blush.gif");
+        images.add("https://c.tenor.com/bEes0xCurvMAAAAC/anime-blush-dizzy.gif");
+        images.add("https://c.tenor.com/hCqcNUuWCf0AAAAC/blush-anime.gif");
+        images.add("https://c.tenor.com/2cWyWrf4ucAAAAAC/cyan-hijirikawa-show-by-rock.gif");
+        images.add("https://c.tenor.com/8-F6-nB1L84AAAAC/anime-blush.gif");
+        images.add("https://c.tenor.com/4KLTabq5i8cAAAAC/bell-blush.gif");
+        images.add("https://c.tenor.com/PYOlVtPLnwoAAAAC/the-quintessential-quintuplets.gif");
+        images.add("https://c.tenor.com/QKuVXBYQ--sAAAAC/blushing-anime.gif");
+        images.add("https://c.tenor.com/yY7EhaDIy8wAAAAd/girl-blush.gif");
+        images.add("https://c.tenor.com/OmrEdg-gq0UAAAAC/anime-blushing.gif");
+        images.add("https://c.tenor.com/4y2F6WB7Z9wAAAAC/tonikaku-kawaii-yuzaki-tsukasa.gif");
+        images.add("https://c.tenor.com/Pyhna-FV8dwAAAAC/blush-kawaii.gif");
 
         embedBuilder.setTitle("**" + member.getEffectiveName() + "is blushing!** :3");
-        //embedBuilder.setImage(images.get(new Random().nextInt(images.size())));
+        embedBuilder.setImage(images.get(new Random().nextInt(images.size())));
         embedBuilder.setColor(ColorTools.getFromRGBString(config.getString("format.color.default")));
         context.getEvent().replyEmbeds(embedBuilder.build()).queue();
 

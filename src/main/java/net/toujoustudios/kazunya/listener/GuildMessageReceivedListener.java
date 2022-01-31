cad0e281ba035e9c275ccac9636da53c35214213
= <ianbour2016@gmail.com>
2022-01-31 22:15:43 +0100

    Improved code

--- src/main/java/net/toujoustudios/kazunya/listener/GuildMessageReceivedListener.java
+++ src/main/java/net/toujoustudios/kazunya/listener/GuildMessageReceivedListener.java
@@ -9,7 +9,8 @@ import net.toujoustudios.kazunya.config.Config;
 import net.toujoustudios.kazunya.main.Main;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * This file has been created by Ian Toujou.
@@ -20,6 +21,8 @@ import java.util.ArrayList;
 public class GuildMessageReceivedListener extends ListenerAdapter {
 
     private final Config config = Config.getDefault();
+    private final List<String> blacklistedWords = List.of("nitro", "nirto", "nitr0", "n1tro", "discrd", "d1scord", "disc0rd");
+    private final List<String> whitelistedSites = List.of("discord.com", "discord.gift", "discord.gg", "tenor.com", "discordapp.net", "discordapp.com");
 
     @Override
     public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
@@ -29,40 +32,33 @@ public class GuildMessageReceivedListener extends ListenerAdapter {
 
         if (baseMessage.contains("http://") || baseMessage.contains("https://")) {
 
-            String checkString = baseMessage.split("http")[1];
+            String[] splits = baseMessage.split(" ");
+            List<String> linksFound = Arrays.stream(splits).filter(s -> s.startsWith("http://") || s.startsWith("https://")).toList();
 
-            if (baseMessage.contains("discord.com/") || baseMessage.contains("discord.gift/") || baseMessage.contains("discord.gg/") || baseMessage.contains("tenor.com/")) {
-                return;
-            }
+            for (String link : linksFound) {
 
-            ArrayList<String> bannedWords = new ArrayList<>();
-            bannedWords.add("nitro");
-            bannedWords.add("nirto");
-            bannedWords.add("nitr0");
-            bannedWords.add("discrd");
-            bannedWords.add("disc0rd");
+                for (String site : whitelistedSites) if (link.contains(site)) return;
 
-            EmbedBuilder embedBuilder = new EmbedBuilder();
-            embedBuilder.setColor(ColorTools.getFromRGBString(config.getString("format.color.default")));
-            embedBuilder.setTitle(":warning: **Message Deleted**");
-            embedBuilder.setDescription("Your message has been deleted for the following reason:\n`Potentially malicious or dangerous links.`\n\n*This message has been automatically reported to administrators.*");
+                for (String site : blacklistedWords) {
+                    if (link.contains(site)) {
 
-            for (String all : bannedWords) {
+                        EmbedBuilder embedBuilder = new EmbedBuilder();
+                        embedBuilder.setColor(ColorTools.getFromRGBString(config.getString("format.color.default")));
+                        embedBuilder.setTitle(":warning: **Message Deleted**");
+                        embedBuilder.setDescription("Your message has been deleted for the following reason:\n`Potentially malicious or dangerous links.`\n\n*This message has been automatically reported to administrators.*");
 
-                if (checkString.contains(all)) {
+                        event.getMessage().delete().queue();
+                        event.getChannel().sendMessage(embedBuilder.build()).queue();
 
-                    event.getMessage().delete().queue();
-                    event.getChannel().sendMessage(embedBuilder.build()).queue();
+                        embedBuilder.setTitle(":warning: **Scam Link Detection:**");
+                        embedBuilder.setDescription("A message was flagged as scam and removed from a server.```" + rawMessage + "```");
+                        User user = Main.getBot().getJDA().getUserById(config.getString("user.admin"));
+                        if (user != null)
+                            user.openPrivateChannel().flatMap(channel -> channel.sendMessage(embedBuilder.build())).queue();
+                        return;
 
-                    embedBuilder.setTitle(":warning: **Scam Link Detection:**");
-                    embedBuilder.setDescription("A message was flagged as scam and removed from a server.```" + rawMessage + "```");
-                    User user = Main.getBot().getJDA().getUserById(config.getString("user.admin"));
-                    if (user != null)
-                        user.openPrivateChannel().flatMap(channel -> channel.sendMessage(embedBuilder.build())).queue();
-                    break;
-
+                    }
                 }
-
             }
 
         }

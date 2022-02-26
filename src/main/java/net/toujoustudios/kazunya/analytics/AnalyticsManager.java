cee245d101ca9606da395fcf4f9e163b42300d02
= <ianbour2016@gmail.com>
2022-02-26 03:22:29 +0100

    ✨ Added AnalyticsManager

---
+++ src/main/java/net/toujoustudios/kazunya/analytics/AnalyticsManager.java
@@ -0,0 +1,12 @@
+package net.toujoustudios.kazunya.analytics;
+
+/**
+ * This file has been created by Ian Toujou.
+ * Project: Kazunya
+ * Date: 26/02/2022
+ * Time: 03:18
+ */
+public class AnalyticsManager {
+
+
+}
---
+++ src/main/java/net/toujoustudios/kazunya/data/AnalyticsDataManager.java
@@ -0,0 +1,12 @@
+package net.toujoustudios.kazunya.data;
+
+/**
+ * This file has been created by Ian Toujou.
+ * Project: Kazunya
+ * Date: 26/02/2022
+ * Time: 02:09
+ */
+public class AnalyticsDataManager {
+
+
+}
